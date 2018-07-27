7fef3081dc5b528d3178bf75dc788c434b55ceeb
hellokingdom <james@hellokingdom.co.uk>
2018-07-27 09:54:31 +0100

    Example showing menu with links off screen

--- src/index.js
+++ src/index.js
@@ -4,7 +4,33 @@ import classNames from './styles.css'
 class Li {
   update(data, index, items, context) {
     const isVisible =
-      index < context.totalTabs ? classNames.isVisible : classNames.isHidden
+      index < context.visibleTabs ? classNames.isVisible : classNames.isHidden
+    if (index === context.activeTab) {
+      this.el = el(
+        'li.' +
+          classNames.tabLayout +
+          '.' +
+          classNames.tabActive +
+          '.' +
+          isVisible,
+        el('a', {
+          text: data.name,
+          href: data.link
+        })
+      )
+    } else {
+      this.el = el(
+        'li.' + classNames.tabLayout + '.' + isVisible,
+        el('a', { text: data.name, href: data.link })
+      )
+    }
+  }
+}
+
+class LiMenu {
+  update(data, index, items, context) {
+    const isVisible =
+      index < context.visibleTabs ? classNames.isHidden : classNames.isVisible
     if (index === context.activeTab) {
       this.el = el(
         'li.' +
@@ -71,32 +97,55 @@ const MorrisonsTabs = (function(tt) {
             link: 'https://my.morrisons.com/'
           },
           {
-            name: 'New Link',
+            name: 'Eat Fresh',
             link: 'https://my.morrisons.com/'
           }
         ]
       }
 
       this.options = extend(defaults, options)
+      this.ulMenu = list('ul', LiMenu)
+      this.ul = list('ul.' + classNames.tabList, Li)
+
+      this.layout = el('div.' + classNames.tabHeader, this.ul)
+      this.layoutMenu = el('div', this.ulMenu)
+
+      mount(document.body, this.layoutMenu, document.body.firstChild)
+      mount(document.body, this.layout, document.body.firstChild)
     }
 
-    TopTabs.prototype.addTabs = function() {
-      const ul = list('ul.' + classNames.tabList, Li)
-      ul.update(this.options.channels, {
+    TopTabs.prototype.updateState = function() {
+      this.ul.update(this.options.channels, {
         activeTab: this.options.activeTab,
-        totalTabs: this.options.totalTabs
+        visibleTabs: Math.trunc(window.innerWidth / 150)
       })
-      const layout = el('div.' + classNames.tabHeader, ul)
+      this.ulMenu.update(this.options.channels, {
+        activeTab: this.options.activeTab,
+        visibleTabs: Math.trunc(window.innerWidth / 150)
+      })
+    }
+
+    TopTabs.prototype.onResize = function() {
       const resize = () => {
-        ul.update(this.options.channels, {
-          activeTab: this.options.activeTab,
-          totalTabs: Math.trunc(window.innerWidth / 150)
-        })
+        this.updateState()
       }
       window.onresize = function(event) {
         resize()
       }
-      mount(document.body, layout, document.body.firstChild)
+    }
+
+    TopTabs.prototype.addMenu = function() {
+      this.ulMenu.update(this.options.channels, {
+        activeTab: this.options.activeTab,
+        totalTabs: this.options.totalTabs
+      })
+    }
+
+    TopTabs.prototype.addTabs = function() {
+      this.ul.update(this.options.channels, {
+        activeTab: this.options.activeTab,
+        totalTabs: this.options.totalTabs
+      })
     }
 
     return TopTabs
@@ -105,6 +154,8 @@ const MorrisonsTabs = (function(tt) {
   tt.init = function(options) {
     const tabs = new tt.TopTabs(options)
     tabs.addTabs()
+    tabs.addMenu()
+    tabs.onResize()
   }
 
   window.morrisonstabs = tt
